e079ca3833a11a2bb079b8cb032dd77c255c3da6
davidrynn <davidrynn@gmail.com>
2015-08-03 23:46:25 -0400

    Added tester image, placehoder image, and improved buttons

--- Simple-Music-Player/DRMusicViewController.m
+++ Simple-Music-Player/DRMusicViewController.m
@@ -9,7 +9,7 @@
 #import "DRMusicViewController.h"
 @import MediaPlayer;
 
-@interface DRMusicViewController () <UITableViewDelegate, UITableViewDataSource>
+@interface DRMusicViewController () <UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate, UISearchControllerDelegate>
 @property (weak, nonatomic) IBOutlet UIView *playerButtonContainer;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (nonatomic, strong) NSDictionary *songsDictionary;
@@ -18,14 +18,17 @@
 @property (nonatomic, strong) NSDictionary *genresArray;
 @property (nonatomic, strong) NSDictionary *playlistsArray;
 @property (nonatomic, strong) NSDictionary *mediaItemsDictionary;
+@property (weak, nonatomic) IBOutlet UIButton *playButton;
+@property (weak, nonatomic) IBOutlet UIButton *backButton;
+@property (weak, nonatomic) IBOutlet UIButton *forwardButton;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedController;
-@property (weak, nonatomic) IBOutlet UIImageView *cellImageView;
 
 @property (nonatomic, strong) MPMusicPlayerController *musicPlayerController;
 @property (nonatomic, strong) MPMediaItem *songToPlay;
 //@property (nonatomic, strong) MPMediaLibrary *musicLibrary;
 @property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
 
+@property (nonatomic, strong) NSArray *searchResults;
 @end
 
 @implementation DRMusicViewController
@@ -75,6 +78,9 @@
     
     
 }
+
+
+# pragma button actions
 - (IBAction)playButtonTapped:(id)sender {
     
     UIButton *button = (UIButton *)sender;
@@ -109,6 +115,68 @@
         
 
 }
+
+
+
+
+- (IBAction)backButtonTapped:(id)sender {
+    UILongPressGestureRecognizer *longTap = [[UILongPressGestureRecognizer alloc] init];
+    [longTap setMinimumPressDuration:1];
+    if ([longTap state] == UIGestureRecognizerStateBegan) {
+        [self.musicPlayerController beginSeekingBackward];
+        NSLog(@"button Held");
+        }
+    if([longTap state] == UIGestureRecognizerStateEnded)
+    {
+        [self.musicPlayerController endSeeking];
+    }
+    else {
+    
+        [self.musicPlayerController skipToBeginning];
+        NSLog(@"Button tapped");
+        
+        [self.musicPlayerController play];
+    
+    }
+    
+}
+
+
+
+
+
+#pragma mark Content Filtering
+//Search Functionality
+- (void)searchBarTextDidEndEditing:(UISearchBar *)searchBar{
+
+
+}
+- (void)filterContentForSearchText:(NSString*)searchText scope:(NSString*)scope
+{
+    NSPredicate *resultPredicate = [NSPredicate predicateWithFormat:@"name contains[c] %@", searchText];
+    self.searchResults = [self.songsDictionary[@"array"] filteredArrayUsingPredicate:resultPredicate];
+}
+
+#pragma mark - UISearchDisplayController Delegate Methods
+-(BOOL)searchController:(UISearchDisplayController *)controller shouldReloadTableForSearchString:(NSString *)searchString {
+    // Tells the table data source to reload when text changes
+    [self filterContentForSearchText:searchString scope:
+     [[self.searchDisplayController.searchBar scopeButtonTitles] objectAtIndex:[self.searchDisplayController.searchBar selectedScopeButtonIndex]]];
+    // Return YES to cause the search result table view to be reloaded.
+    return YES;
+}
+
+-(BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchScope:(NSInteger)searchOption {
+    // Tells the table data source to reload when scope bar selection changes
+    [self filterContentForSearchText:self.searchDisplayController.searchBar.text scope:
+     [[self.searchDisplayController.searchBar scopeButtonTitles] objectAtIndex:searchOption]];
+    // Return YES to cause the search result table view to be reloaded.
+    return YES;
+}
+
+
+
+//End Search Function
 
 
 - (void)didReceiveMemoryWarning {
@@ -194,9 +262,18 @@
         MPMediaItem *item = (MPMediaItem *) self.mediaItemsDictionary[@"array"][indexPath.row];
         
         cell.textLabel.text =[NSString stringWithFormat:@"%@", item.title];
-        cell.detailTextLabel.text = [NSString stringWithFormat:@"%@, %@", item.artist, item.albumTitle];
+        cell.detailTextLabel.text = [NSString stringWithFormat:@"%@ -- %@", item.artist, item.albumTitle];
+        
+        if (!item.artwork) {
+            cell.imageView.image = [UIImage imageNamed:@"Icon-Small"];
+        }
+        else
+        {
         UIImage *albumArtWork = [item.artwork imageWithSize:CGSizeMake(cell.frame.size.height, cell.frame.size.height)];
+
         cell.imageView.image =albumArtWork;
+        }
+
     }
     //    if (![self.mediaItemsDictionary[@"category"][indexPath.row] isEqualToString:@"Songs"]) {
     //
@@ -213,6 +290,7 @@
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     self.songToPlay = (MPMediaItem *) self.mediaItemsDictionary[@"array"][indexPath.row];
+    [self playButtonTapped:self.playButton];
     
 }
 
