c88267502fbf2d4d4796d0dae7aa9d3bc7f4d3b0
davidrynn <davidrynn@gmail.com>
2015-09-02 14:10:42 -0400

    git wants me to save here

--- Simple-Music-Player/DRMediaTableViewController.m
+++ Simple-Music-Player/DRMediaTableViewController.m
@@ -50,7 +50,7 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
-#warning Incomplete method implementation.
+
     // Configure the cell...
     MPMediaItem *item = self.songs[indexPath.row];
     cell.textLabel.text = [NSString stringWithFormat:@"%@ -- %@",item.title, item.artist];
