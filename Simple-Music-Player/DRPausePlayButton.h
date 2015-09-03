59f2420578d1e0012ba97907df9423febcf098bf
davidrynn <davidrynn@gmail.com>
2015-09-03 01:05:27 -0400

    added container view successfully and button subclass

---
+++ Simple-Music-Player/DRPausePlayButton.h
@@ -0,0 +1,13 @@
+//
+//  DRPausePlayButton.h
+//  Simple-Music-Player
+//
+//  Created by David Rynn on 9/3/15.
+//  Copyright (c) 2015 David Rynn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DRPausePlayButton : UIButton
+
+@end
